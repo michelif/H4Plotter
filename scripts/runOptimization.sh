43e7d7e9fea2c59516af0171f7a0cf915f0b0f4e
michelif <francesco.micheli@cern.ch>
2016-06-27 17:52:40 +0200

    first commit

---
+++ scripts/runOptimization.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for iStep in `seq 0 10`;
+do
+    echo "starting step "$iStep
+    iStepPlusOne=$((iStep+1))
+    rm data/optStep$iStepPlusOne.txt
+    for i in `seq 1 11`;
+    do
+	echo $i
+	./bin/intercalibrateChannels 4683 xtal11 $i $iStep &> data/$i.dat
+	cat data/$i.dat |grep minimizer|awk '{print $6}'>>data/optStep$iStepPlusOne.txt
+    done    
+    echo "done step "$iStep
+
+
+done        
