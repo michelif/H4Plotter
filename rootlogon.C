b028b5cdc4a190af1ade6c640ef4c7e2379c6f41
michelif <francesco.micheli@cern.ch>
2016-08-02 14:15:22 +0200

    fixing environment setup

---
+++ rootlogon.C
@@ -0,0 +1,4 @@
+{
+    gSystem->Load("/afs/cern.ch/user/m/micheli/scratch1/H4Analysis_2016_2/CfgManager/lib/CfgManagerDict.so");
+    gStyle->SetPalette(51);
+}
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
 source /afs/cern.ch/sw/lcg/external/gcc/4.9/x86_64-slc6-gcc49-opt/setup.sh
-source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.05.02/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
+#source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.05.02/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
+#source /afs/cern.ch/sw/lcg/releases/LCG_84/Python/2.7.10/x86_64-slc6-gcc49-opt/Python-env.sh 
+#source /afs/cern.ch/sw/lcg/releases/LCG_84/ROOT/6.06.02/x86_64-slc6-gcc49-opt/bin/thisroot.sh 
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.06.06/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh 
 
-export LD_LIBRARY_PATH=/afs/cern.ch/user/m/micheli/scratch1/H4Analysis_2016/DynamicTTree/lib/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/afs/cern.ch/user/m/micheli/scratch1/H4Analysis_2016/CfgManager/lib/:$LD_LIBRARY_PATH
+#export PATH=t/bin/:$PATH
+export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/app/releases/ROOT/6.06.06/x86_64-slc6-gcc49-opt/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/rh/python27/root/usr/lib64/:$LD_LIBRARY_PATH
+
+#export PYTHON_PATH=/afs/cern.ch/user/m/micheli/scratch1/H4Analysis_2016_2/lib/:$PYTHON_PATH
+export PYTHON_PATH=/afs/cern.ch/user/m/micheli/scratch1/H4Analysis_2016_2/lib/:$PYTHON_PATH
+export PYTHON_PATH=/opt/rh/python27/root/usr/lib64/:$PYTHON_PATH
+
+export LD_LIBRARY_PATH=/afs/cern.ch/user/m/micheli/scratch1/H4Analysis_2016_2/DynamicTTree/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/afs/cern.ch/user/m/micheli/scratch1/H4Analysis_2016_2/CfgManager/lib/:$LD_LIBRARY_PATH
