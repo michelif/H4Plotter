233bb0f2ba97917bd35f9ab3bc8e6b3ac7ce7d62
michelif <francesco.micheli@cern.ch>
2016-09-12 15:42:56 +0200

    adding 4apd

--- scripts/runOptimization.sh
+++ scripts/runOptimization.sh
@@ -9,8 +9,10 @@ do
     do
 	echo $i
 #	./bin/intercalibrateChannels 4683 xtal11 $i $iStep &> data/$i.dat
-	./bin/intercalibrateChannels 4683 xtal11_2 $i $iStep &> data/$i.dat
-	cat data/$i.dat |grep minimizer|awk '{print $6}'>>data/optStep$iStepPlusOne.txt
+#	./bin/intercalibrateChannels 4695 xtal11_2 $i $iStep &> data/$i.dat
+	./bin/intercalibrateChannels 4682 xtal4apd $i $iStep &> data/$i.dat
+#	cat data/$i.dat |grep minimizer|awk '{print $6}'>>data/optStep$iStepPlusOne.txt
+	cat data/$i.dat |grep minimizer|awk '{print $6}'>>data/opt4apdStep$iStepPlusOne.txt
     done    
     echo "done step "$iStep
 
